e77b827f56e566f1d583f4ee8bddfa878c7899ef
JAndersen13 <jowens@byupathway.edu>
2025-07-08 17:58:31 -0700

    week 02 BOM activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') { 
+        
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.textContent = input.value + " ";
+        li.appendChild(deleteButton);
+
+        list.appendChild(li);
+
+        input.value = "";
+        input.focus();
+    }
+});
+
